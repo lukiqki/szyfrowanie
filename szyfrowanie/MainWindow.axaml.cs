9bc01327cb602d1830e316a4e71754a2a913fbbe
lukiqki <lukaszch980@gmail.com>
2025-12-01 22:45:46 +0100

    .

---
+++ szyfrowanie/MainWindow.axaml.cs
@@ -0,0 +1,50 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace szyfrowanie;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private string SzyfrCezara(string tekst, int przesuniecie)
+    {
+        char[] zaszyfrowanyTekst = new char[tekst.Length];
+
+        for (int i = 0; i < tekst.Length; i++)
+        {
+            char znak = tekst[i];
+
+
+            if (znak >= 'a' && znak <= 'z')
+            {
+                zaszyfrowanyTekst[i] = (char)((((znak - 'a') + przesuniecie) % 26 + 26) % 26 + 'a');
+            }
+            else
+            {
+                zaszyfrowanyTekst[i] = znak;
+            }
+        }
+        return new string(zaszyfrowanyTekst);
+    }
+
+    private void ZaszyfrujClick(object sender, RoutedEventArgs e)
+    {
+        string tekstDoZaszyfrowania = wprowadzonytext.Text;
+        string kluczstr = kluczszyfrowania.Text;
+
+        if (int.TryParse(kluczstr, out int klucz))
+        {
+            string zaszyfrowany = SzyfrCezara(tekstDoZaszyfrowania, klucz);
+
+            zaszyfrowanytext.Text = zaszyfrowany;
+        }
+        else
+        {
+            zaszyfrowanytext.Text = "wprowadź poprawny klucz";
+        }
+    }
+}
